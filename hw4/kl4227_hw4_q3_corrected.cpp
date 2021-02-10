a9dd56612cb13bd9309d48ce6802f4512fd0e029
tomjackson-tech <76239419+tomjackson-tech@users.noreply.github.com>
2021-02-10 23:49:18 +0800

    upload corrected q3

---
+++ hw4/kl4227_hw4_q3_corrected.cpp
@@ -0,0 +1,43 @@
+/*
+ * author: Lin, Kuan-You
+ * date: 2/3/2021
+ * 2021 NYU Tandon Bridge Homework 4 Question 3 {corrected}
+
+Write a program that reads from the user a positive integer (in a decimal representation),
+and prints its binary (base 2) representation.
+
+ Your program should interact with the user exactly as it shows in the following example:
+Enter decimal number:
+76
+The binary representation of 76 is 1001100
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Enter decimal number: " << endl;
+    int decimal{0};
+    cin >> decimal;
+    int decimal_value = decimal;
+
+    // Because we're storing the binary number in an unsigned long long type,
+    // it is possible that an overflow would occur if the user enter a super large
+    // decimal integer.
+    unsigned long long binary{0};
+    unsigned long long base{1};
+
+    // using the method similar to the synthetic division
+    while (decimal > 0){
+        unsigned long long temp = decimal % 2;
+        temp *= base;
+        binary += temp;
+        base *= 10;
+        decimal /= 2;
+    }
+    binary %= base;
+
+    cout << "The binary representation of " << decimal_value << " is " <<binary;
+    return 0;
+}
